515189d81b4ce7a70f76a3cbb5f028f3670266b4
fahim chowdhury <fahim.chowdhury1985@gmail.com>
2018-04-27 22:38:17 +0100

    styling

--- src/components/app/app.vue
+++ src/components/app/app.vue
@@ -1,4 +1,4 @@
 <template src="./template/template.html">
 </template><script src="./src/component.js">
-</script><style lang="scss" scoped src="./style/style.scss"></style>
+</script><style lang="scss" src="./style/style.scss"></style>
 
--- src/components/splash/src/component.js
+++ src/components/splash/src/component.js
@@ -31,7 +31,6 @@ export default {
   methods:{
       onLoaded(data){
           Data.collection = data.items;
-
           setTimeout(() => {
               router.push('home');   
           }, 1000);
