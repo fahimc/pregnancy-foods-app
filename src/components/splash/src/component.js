e5df68674ca33019b9f2afcaa65c194e342c27aa
fahim chowdhury <fahim.chowdhury1985@gmail.com>
2018-04-28 11:40:49 +0100

    loading data from a spreadsheet

--- src/components/splash/src/component.js
+++ src/components/splash/src/component.js
@@ -4,39 +4,77 @@ import router from '../../../router/index';
 export default {
   name: 'splash',
   mounted() {
-    let url = 'static/data/data.json';
-
-    let xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = ()=> {
-              if (xhr.readyState == 4) {
-                    if (xhr.status == 200 || xhr.status == 0) {
-                          try {
-                              this.onLoaded(JSON.parse(xhr.responseText));
-                          } catch (e) {
-                              this.onError(e);
-                                return;
-                          }
-                    } else {
-                        this.onError(xhr.status);
-                    }
-              }
-        }
-        try {
-              xhr.open("GET", url, true);
-              xhr.send();
-        } catch (e) {
-            this.onError(e);
-        }   
+    this.loadSpreadsheet();
   },
-  methods:{
-      onLoaded(data){
-          Data.collection = data.items;
-          setTimeout(() => {
-              router.push('home');   
-          }, 1000);
-      },
-      onErro(){
+  methods: {
+    loadLocalFile() {
+      let url = 'static/data/data.json';
 
+      let xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200 || xhr.status == 0) {
+            try {
+              this.onLoaded(JSON.parse(xhr.responseText));
+            } catch (e) {
+              this.onError(e);
+              return;
+            }
+          } else {
+            this.onError(xhr.status);
+          }
+        }
       }
+      try {
+        xhr.open("GET", url, true);
+        xhr.send();
+      } catch (e) {
+        this.onError(e);
+      }  
+    },
+    loadSpreadsheet() {
+      Tabletop.init({
+        key: 'https://docs.google.com/spreadsheets/d/1388DmdneL72roKI0Xn2JBJFqbbbu9N3LQvYckKHyHyY/edit?usp=sharing',
+        callback: this.onSpreadsheetLoaded.bind(this),
+        simpleSheet: true
+      })
+
+    },
+    onSpreadsheetLoaded(data, tabletop) {
+      let items = [];
+        data.forEach((row,index) => {
+            let item = {
+                name: this.getValueByString(row,'name'),
+                source: this.getValueByString(row, 'source'),
+                alias: this.getValueByString(row, 'alias').split(','),
+                verdict: this.getValueByString(row, 'verdict'),
+                snippet: this.getValueByString(row, 'important'),
+                description: this.getValueByString(row, 'description').replace(/\\n/g, ''),
+                safeCollection: this.getValueByString(row, 'safe').split(','),
+                avoidCollection: this.getValueByString(row, 'avoid').split(',')
+            }
+            items.push(item);
+        });
+        this.onLoaded({
+            items:items
+        });
+    },
+    getValueByString(item,keyPart){
+        for(let key in item){
+            if (key.toLowerCase().indexOf(keyPart.toLowerCase()) >= 0)
+            {
+                return item[key];
+            }
+        }
+    },
+    onLoaded(data) {
+      Data.collection = data.items;
+      setTimeout(() => {
+        router.push('home');
+      }, 1000);
+    },
+    onErro() {
+
+    }
   }
 };
--- src/main.js
+++ src/main.js
@@ -12,5 +12,7 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: {
+    App
+  }
 })
